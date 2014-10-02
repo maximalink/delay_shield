eab25fbbd1ac5a5283d18758c0c9b01b06048299
Jozsef Nyitrai <nyitrai.jozsef@gmail.com>
2014-10-02 10:46:45 +0200

    IE 8 compatibility fix

--- ticket_count_back.php
+++ ticket_count_back.php
@@ -1,5 +1,8 @@
+<!DOCTYPE html>
 <html ng-app="backCounter">
 <head>
+  <meta charset='UTF-8' />
+  <meta http-equiv="X-UA-Compatible" content="IE-edge" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
 
